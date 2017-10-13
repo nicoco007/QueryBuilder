9151aa6880be5503b3528f46071c7231107c278b
Nicolas Gnyra <nicolas@gnyra.com>
2017-10-12 23:45:01 -0400

    Add test for DELETE query

--- tests/DeleteQueryTest.php
+++ tests/DeleteQueryTest.php
@@ -3,8 +3,8 @@
 require_once __DIR__ . '/QueryBuilderTest.php';
 
 use QueryBuilder\ColumnStatement;
+use QueryBuilder\ConditionCollection;
 use QueryBuilder\DeleteQuery;
-use QueryBuilder\InvalidQueryException;
 use QueryBuilder\RawValueStatement;
 
 final class DeleteQueryTest extends QueryBuilderTest {
@@ -15,8 +15,29 @@ final class DeleteQueryTest extends QueryBuilderTest {
             ->setLimit(10)
             ->build();
 
-        $this->printResults($built);
+        $this->assertEquals("DELETE FROM `users` ORDER BY `confirmed` ASC, `username` DESC LIMIT 10", $built->getString());
+        $this->assertEquals([], $built->getParameters());
 
-        $this->markTestIncomplete();
+        $this->printResults($built);
+    }
+
+    public function testAllSettings() {
+        $conditions = (new ConditionCollection(OPERATOR_AND))
+            ->addCondition(new ColumnStatement('confirmed'), new RawValueStatement(false));
+
+        $built = (new DeleteQuery('users'))
+            ->addOrder('confirmed')
+            ->addOrder('username', ORDER_DESC)
+            ->setLimit(10)
+            ->setLowPriority(true)
+            ->setIgnore(true)
+            ->setQuick(true)
+            ->setConditionCollection($conditions)
+            ->build();
+
+        $this->assertEquals("DELETE LOW_PRIORITY QUICK IGNORE FROM `users` WHERE `confirmed` = ? ORDER BY `confirmed` ASC, `username` DESC LIMIT 10", $built->getString());
+        $this->assertEquals([false], $built->getParameters());
+
+        $this->printResults($built);
     }
 }
