017c31d9d3f4ba97e19d4d27c700e3d11b214b88
Nicolas Gnyra <nicolas@gnyra.com>
2018-08-04 15:07:19 -0400

    Make priority setting less dumb

---
+++ src/QueryBuilder/QueryPriority.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace QueryBuilder;
+
+
+class QueryPriority
+{
+    const NONE = 0;
+    const LOW = 1;
+    const HIGH = 2;
+}
--- tests/InsertQueryTest.php
+++ tests/InsertQueryTest.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/QueryBuilderTest.php';
 use QueryBuilder\ColumnStatement;
 use QueryBuilder\InsertQuery;
 use QueryBuilder\InvalidQueryException;
+use QueryBuilder\QueryPriority;
 use QueryBuilder\RawValueStatement;
 
 final class InsertQueryTest extends QueryBuilderTest {
@@ -30,7 +31,7 @@ final class InsertQueryTest extends QueryBuilderTest {
     public function testLowPriority() {
         $built = (new InsertQuery('users'))
             ->addAssignment(new ColumnStatement('confirmed'), new RawValueStatement(true))
-            ->setLowPriority(true)
+            ->setPriority(QueryPriority::LOW)
             ->build();
 
         $this->assertEquals('INSERT LOW_PRIORITY INTO `users` SET `confirmed` = ?', $built->getString());
@@ -41,31 +42,11 @@ final class InsertQueryTest extends QueryBuilderTest {
     public function testHighPriority() {
         $built = (new InsertQuery('users'))
             ->addAssignment(new ColumnStatement('confirmed'), new RawValueStatement(true))
-            ->setHighPriority(true)
+            ->setPriority(QueryPriority::HIGH)
             ->build();
 
         $this->assertEquals('INSERT HIGH_PRIORITY INTO `users` SET `confirmed` = ?', $built->getString());
 
         $this->printResults($built);
-    }
-
-    public function testLowAndHighPriorityShouldThrowException() {
-        $this->expectException(InvalidQueryException::class);
-        $this->expectExceptionMessage('Query cannot be both high and low priority');
-
-        (new InsertQuery('users'))
-            ->setLowPriority(true)
-            ->setHighPriority(true)
-            ->build();
-    }
-
-    public function testHighAndLowPriorityShouldThrowException() {
-        $this->expectException(InvalidQueryException::class);
-        $this->expectExceptionMessage('Query cannot be both low and high priority');
-
-        (new InsertQuery('users'))
-            ->setHighPriority(true)
-            ->setLowPriority(true)
-            ->build();
     }
 }
